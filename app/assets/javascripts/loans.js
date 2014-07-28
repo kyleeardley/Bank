68d9cbdd00381f229ad99aae2d96a7d392163bb6
kyleeardley <kyleeardley@gmail.com>
2014-07-27 21:08:41 -0600

    9:08

--- app/assets/javascripts/loans.js
+++ app/assets/javascripts/loans.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-	$('#datepicker').datepicker({format: 'yyyy-mm-dd'});
+$(document).on('page:change', function () {
+ $('#datepicker').datepicker({format: 'yyyy-mm-dd'});
+});
+
+
+//$(document).ready(function() {
+//	$('#datepicker').datepicker({format: 'yyyy-mm-dd'});
 	
-});	
+//});	
 
 	
